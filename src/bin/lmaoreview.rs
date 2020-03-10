d38013e5e8578cdd91b77a0905e27a5269d3bf16
msizanoen1 <qtmlabs@protonmail.com>
2020-03-10 16:24:51 +0700

    Clean up review display

--- src/bin/lmaoreview.rs
+++ src/bin/lmaoreview.rs
@@ -1,5 +1,6 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::QueryResult;
 use dotenv::dotenv;
 use exitfailure::ExitFailure;
 use failure::ResultExt;
@@ -8,6 +9,34 @@ use lmaobgd::models::*;
 use lmaobgd::schema::*;
 use std::env;
 use std::io::stdin;
+
+fn process_question(q: &str) -> String {
+    q.lines()
+        .map(|s| s.trim())
+        .filter(|s| *s != "")
+        .filter(|s| {
+            !s.starts_with("A:")
+                && !s.starts_with("B:")
+                && !s.starts_with("C:")
+                && !s.starts_with("D:")
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+fn get_question_string(db: &PgConnection, id: i32) -> QueryResult<String> {
+    Ok(process_question(&actions::get_question_string(db, id)?))
+}
+
+fn get_answer_string(db: &PgConnection, id: i32) -> QueryResult<String> {
+    let answer = actions::get_answer_string(db, id)?;
+    Ok(answer
+        .lines()
+        .map(|s| s.trim())
+        .filter(|s| *s != "")
+        .collect::<Vec<_>>()
+        .join("\n"))
+}
 
 fn main() -> Result<(), ExitFailure> {
     let _ = dotenv();
@@ -19,33 +48,33 @@ fn main() -> Result<(), ExitFailure> {
         .context("unable to get unreviewed data")?;
     for answer in unreviewed {
         let question_id = answer.question_id;
-        let question = actions::get_question_string(&db, question_id)?;
+        let question = get_question_string(&db, question_id)?;
         println!("Question {} ({}):", question_id, question);
         println!("Possible answers:");
         println!(
             "{} ({})",
             answer.answer1,
-            actions::get_answer_string(&db, answer.answer1)?
+            get_answer_string(&db, answer.answer1)?
         );
         println!(
             "{} ({})",
             answer.answer2,
-            actions::get_answer_string(&db, answer.answer2)?
+            get_answer_string(&db, answer.answer2)?
         );
         println!(
             "{} ({})",
             answer.answer3,
-            actions::get_answer_string(&db, answer.answer3)?
+            get_answer_string(&db, answer.answer3)?
         );
         println!(
             "{} ({})",
             answer.answer4,
-            actions::get_answer_string(&db, answer.answer4)?
+            get_answer_string(&db, answer.answer4)?
         );
         println!(
             "Answer used: {} ({})",
             answer.answer_used,
-            actions::get_answer_string(&db, answer.answer_used)?
+            get_answer_string(&db, answer.answer_used)?
         );
         loop {
             print!(
@@ -73,9 +102,9 @@ fn main() -> Result<(), ExitFailure> {
                 println!(
                     "Updated question {} ({}) to {} ({})",
                     answer.question_id,
-                    actions::get_question_string(&db, answer.question_id)?,
+                    get_question_string(&db, answer.question_id)?,
                     input,
-                    actions::get_answer_string(&db, input)?
+                    get_answer_string(&db, input)?
                 );
             } else if input == 2 {
                 diesel::delete(answers::table.find(answer.question_id)).execute(&db)?;
