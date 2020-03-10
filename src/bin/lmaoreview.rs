bceca1f76d2629fd976a038307969821f8cc8b9d
msizanoen1 <qtmlabs@protonmail.com>
2020-03-10 17:17:55 +0700

    Fix cleanup

--- src/bin/lmaoreview.rs
+++ src/bin/lmaoreview.rs
@@ -11,18 +11,22 @@ use std::env;
 use std::io::stdin;
 
 fn process_question(q: &str) -> String {
-    let mut vec = q
-        .lines()
-        .map(|s| s.trim())
-        .filter(|s| *s != "")
-        .filter(|s| {
-            !s.starts_with("A:")
-                && !s.starts_with("B:")
-                && !s.starts_with("C:")
-                && !s.starts_with("D:")
-        })
-        .collect::<Vec<_>>();
-    vec.truncate(vec.len() - 4);
+    let (mut vec, n) = q.lines().map(|s| s.trim()).filter(|s| *s != "").fold(
+        (Vec::new(), 0),
+        |(mut acc, i), e| {
+            if e.starts_with("A:")
+                || e.starts_with("B:")
+                || e.starts_with("C:")
+                || e.starts_with("D:")
+            {
+                (acc, i + 1)
+            } else {
+                acc.push(e);
+                (acc, i)
+            }
+        },
+    );
+    vec.truncate(vec.len() - n);
     let mut iter = vec.into_iter();
     format!(
         "{}. {}",
