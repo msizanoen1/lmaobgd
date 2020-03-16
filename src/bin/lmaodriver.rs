0c4d13bcf4602014cbfb7f886cc1a32c204cefe8
msizanoen1 <qtmlabs@protonmail.com>
2020-03-16 20:27:43 +0700

    real space bar

--- src/bin/lmaodriver.rs
+++ src/bin/lmaodriver.rs
@@ -176,14 +176,14 @@ async fn main() -> Result<(), exitfailure::ExitFailure> {
             }
             answers[idx] = a_id;
             if cur_answer == Some(a_id) {
-                wd.run_script_elem("arguments[0].click();", &input).await?;
+                wd.element_send_keys(&input, "\u{e00d}").await?;
                 answered = true;
             }
         }
         if !answered {
             let idx = rand::thread_rng().gen_range(0, 4);
             unknowns.insert(q_id, answers[idx]);
-            wd.run_script_elem("arguments[0].click();", &input_elems[idx]).await?;
+            wd.element_send_keys(&input_elems[idx], "\u{e00d}").await?;
         }
 
         answer_of_questions.insert(q_id, answers);
