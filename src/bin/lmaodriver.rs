65576ed6bf6a054898e3b0ff70060e8370ea2a99
msizanoen1 <qtmlabs@protonmail.com>
2020-03-21 16:50:43 +0700

    Revert back to own webdriver

--- src/bin/lmaodriver.rs
+++ src/bin/lmaodriver.rs
@@ -1,85 +1,21 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
-use failure::Error;
 use lmaobgd::actions;
 use lmaobgd::models::*;
 use lmaobgd::schema::*;
+use lmaobgd::webdriver::*;
 use rand::prelude::*;
 use std::collections::HashMap;
 use std::iter::once;
 use std::mem::take;
-use std::process::Stdio;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
 use structopt::StructOpt;
-use thirtyfour::common::scriptargs::ScriptArgs;
-use thirtyfour::{By, Capabilities, DesiredCapabilities, Keys, WebDriver, WebDriverCommands};
-use tokio::io::{copy, stderr, BufReader};
-use tokio::net::TcpListener;
-use tokio::prelude::*;
-use tokio::process::{Child, Command};
 use tokio::signal::ctrl_c;
-use tokio::stream::StreamExt;
 use tokio::task::spawn_blocking;
 use tokio::time::delay_for;
-
-fn wd_error(e: thirtyfour::error::WebDriverError) -> Error {
-    failure::format_err!("WebDriver error: {:?}", e)
-}
-
-async fn new_firefox<T>(
-    command: Option<T>,
-    headless: bool,
-    verbose: bool,
-) -> Result<(WebDriver, Child), Error>
-where
-    T: Into<String>,
-{
-    let command = command
-        .map(|x| x.into())
-        .unwrap_or_else(|| String::from("geckodriver"));
-    let sa = std::net::SocketAddr::from(([0, 0, 0, 0], 0));
-    let port = TcpListener::bind(sa).await?.local_addr()?.port();
-    let url = format!("http://127.0.0.1:{}", port);
-    let mut child = Command::new(command)
-        .arg("-v")
-        .arg("-p")
-        .arg(&port.to_string())
-        .stdout(Stdio::piped())
-        .spawn()?;
-    let mut child_stderr = BufReader::new(child.stdout.take().unwrap());
-    let mut lines = (&mut child_stderr).lines();
-    loop {
-        tokio::select! {
-            line = lines.next() => {
-                if let Some(line) = line {
-                    let line = line?;
-                    if line.contains("Listening") && line.contains(&port.to_string()) {
-                        if verbose {
-                            tokio::spawn(async move {
-                                let _ = copy(&mut child_stderr, &mut stderr()).await;
-                            });
-                        }
-                        let mut caps = DesiredCapabilities::firefox();
-                        let inner = caps.get_mut().as_object_mut().unwrap();
-                        inner.remove("platform");
-                        inner.remove("version");
-                        if headless {
-                            caps.add_firefox_option("args", ["-headless"]).map_err(wd_error)?;
-                        }
-                        let wd = WebDriver::new(&url, &caps).await.map_err(wd_error)?;
-                        return Ok((wd, child));
-                    }
-                }
-            }
-            _ = &mut child => {
-                failure::bail!("Child errored");
-            }
-        }
-    }
-}
 
 fn num_list(data: &str) -> Vec<usize> {
     let (list, st) = data
@@ -152,9 +88,6 @@ struct Args {
     /// Geckodriver command to use
     #[structopt(short, long)]
     geckodriver: Option<String>,
-    /// Verbose mode
-    #[structopt(short, long)]
-    verbose: bool,
     /// Account ID
     id: String,
     /// URL to navigate
@@ -169,28 +102,17 @@ async fn main() -> Result<(), exitfailure::ExitFailure> {
     let db = Arc::new(Mutex::new(
         spawn_blocking(move || PgConnection::establish(&url)).await??,
     ));
-    let (wd, mut child) =
-        new_firefox(args.geckodriver.as_ref(), args.headless, args.verbose).await?;
+    let wd = WebDriver::new_firefox(args.geckodriver.as_ref(), args.headless).await?;
 
     let main = async {
-        wd.get("http://study.hanoi.edu.vn/dang-nhap?returnUrl=/")
-            .await
-            .map_err(wd_error)?;
-        let username = wd
-            .find_element(By::Id("UserName"))
-            .await
-            .map_err(wd_error)?;
-        let password = wd
-            .find_element(By::Id("Password"))
-            .await
-            .map_err(wd_error)?;
-        username.send_keys(&args.id).await.map_err(wd_error)?;
-        password.send_keys(&args.id).await.map_err(wd_error)?;
-        let button = wd
-            .find_element(By::Id("AjaxLogin"))
-            .await
-            .map_err(wd_error)?;
-        button.click().await.map_err(wd_error)?;
+        wd.navigate("http://study.hanoi.edu.vn/dang-nhap?returnUrl=/")
+            .await?;
+        let username = wd.get_element(Using::CssSelector, "#UserName").await?;
+        let password = wd.get_element(Using::CssSelector, "#Password").await?;
+        wd.element_send_keys(&username, &args.id).await?;
+        wd.element_send_keys(&password, &args.id).await?;
+        let button = wd.get_element(Using::CssSelector, "#AjaxLogin").await?;
+        wd.element_click(&button).await?;
 
         while run(&wd, &args, Arc::clone(&db)).await? {
             // repeat
@@ -209,9 +131,8 @@ async fn main() -> Result<(), exitfailure::ExitFailure> {
     };
 
     if !args.no_autoclose {
-        wd.close().await.map_err(wd_error)?;
+        wd.close().await?;
     }
-    child.kill()?;
 
     Ok(())
 }
@@ -224,22 +145,17 @@ async fn run(
     let test_url = &args.test_url;
     let db2 = Arc::clone(&db);
     let data = spawn_blocking(move || actions::js_get_data(&db2.lock().unwrap())).await??;
-    wd.get(test_url).await.map_err(wd_error)?;
-    let start = wd
-        .find_element(By::Id("start-test"))
-        .await
-        .map_err(wd_error)?;
-    start.click().await.map_err(wd_error)?;
+    wd.navigate(test_url).await?;
+    let start = wd.get_element(Using::CssSelector, "#start-test").await?;
+    wd.element_click(&start).await?;
     let title_elem = wd
-        .find_element(By::Css("body .row .col-12 h1"))
-        .await
-        .map_err(wd_error)?;
+        .get_element(Using::CssSelector, "body .row .col-12 h1")
+        .await?;
     let id_elem = wd
-        .find_element(By::Css("body .row .row .col-12 div"))
-        .await
-        .map_err(wd_error)?;
-    let title = title_elem.text().await.map_err(wd_error)?;
-    let id_str = id_elem.text().await.map_err(wd_error)?;
+        .get_element(Using::CssSelector, "body .row .row .col-12 div")
+        .await?;
+    let title = wd.get_element_text(&title_elem).await?;
+    let id_str = wd.get_element_text(&id_elem).await?;
     let id = id_str
         .rsplit(':')
         .nth(0)
@@ -248,69 +164,55 @@ async fn run(
         .unwrap_or(0);
     println!("Test name: {}", title);
     println!("Test ID: {}", id);
-    let questions = wd
-        .find_elements(By::Css(".question-box"))
-        .await
-        .map_err(wd_error)?;
+    let questions = wd.get_elements(Using::CssSelector, ".question-box").await?;
     let mut question_maps = HashMap::new();
     let mut answer_of_questions = HashMap::new();
     let mut answer_maps = HashMap::new();
     let mut unknowns = HashMap::new();
     let mut question_ids = Vec::new();
     for question in questions {
-        let q_id = question
-            .get_attribute("data-id")
-            .await
-            .map_err(wd_error)?
+        let q_id = wd
+            .get_element_attr(&question, "data-id")
+            .await?
             .parse::<i32>()?;
         question_ids.push(q_id);
         let cur_answer = data.get(&q_id).copied();
         if args.force_fetch || cur_answer.is_none() {
-            let q_text = question.text().await.map_err(wd_error)?;
+            let q_text = wd.get_element_text(&question).await?;
             println!("Question {}: {}", q_id, process_question(&q_text));
             question_maps.insert(q_id, q_text);
         }
-        let inputs = question
-            .find_elements(By::Css(r#"input[type="radio"]"#))
-            .await
-            .map_err(wd_error)?;
+        let inputs = wd
+            .get_elements_from_element(&question, Using::CssSelector, r#"input[type="radio"]"#)
+            .await?;
         let mut answers = Vec::new();
         let mut input_elems = Vec::new();
         let mut answered = false;
         for input in inputs {
             input_elems.push(input.clone());
-            let a_id = input
-                .get_property("value")
-                .await
-                .map_err(wd_error)?
+            let a_id = wd
+                .get_element_prop::<String>(&input, "value")
+                .await?
                 .parse::<i32>()?;
             if cur_answer.is_none() || args.force_fetch {
-                let mut sa = ScriptArgs::new();
-                sa.push(input.clone()).map_err(wd_error)?;
                 let a_text = wd
-                    .execute_script_with_args(
+                    .run_script_elem(
                         "return arguments[0].parentNode.parentNode.innerText;",
-                        &sa,
+                        &input,
                     )
-                    .await
-                    .map_err(wd_error)?
-                    .convert()
-                    .map_err(wd_error)?;
+                    .await?;
                 answer_maps.insert(a_id, a_text);
             }
             answers.push(a_id);
             if cur_answer == Some(a_id) {
-                input.send_keys(Keys::Space).await.map_err(wd_error)?;
+                wd.element_send_keys(&input, "\u{e00d}").await?;
                 answered = true;
             }
         }
         if !answered {
             let idx = rand::thread_rng().gen_range(0, answers.len());
             unknowns.insert(q_id, answers[idx]);
-            input_elems[idx]
-                .send_keys(Keys::Space)
-                .await
-                .map_err(wd_error)?;
+            wd.element_send_keys(&input_elems[idx], "\u{e00d}").await?;
         }
 
         answer_of_questions.insert(q_id, answers);
@@ -318,20 +220,16 @@ async fn run(
     let mut correct: Option<Vec<i32>> = None;
     let mut has_incorrect = false;
     if !args.no_submit {
-        wd.execute_script(r#"SendUserTestResultToServer("Đang nộp bài, vui lòng đợi và không thực hiện thêm bất cứ thao tác nào!", 2);"#).await.map_err(wd_error)?;
+        wd.run_script_unit(r#"SendUserTestResultToServer("Đang nộp bài, vui lòng đợi và không thực hiện thêm bất cứ thao tác nào!", 2);"#).await?;
         println!("Waiting for result page...");
         delay_for(Duration::from_secs(15)).await;
         if args.autoreview {
-            let correct_e = wd
-                .find_element(By::Css("#lblTrueAnswer"))
-                .await
-                .map_err(wd_error)?;
+            let correct_e = wd.get_element(Using::CssSelector, "#lblTrueAnswer").await?;
             let wrong = wd
-                .find_element(By::Css("#lblFalseAnser")) // intentional typo
-                .await
-                .map_err(wd_error)?;
-            let correct_t = correct_e.text().await.map_err(wd_error)?;
-            let wrong = wrong.text().await.map_err(wd_error)?;
+                .get_element(Using::CssSelector, "#lblFalseAnser") // intentional typo
+                .await?;
+            let correct_t = wd.get_element_text(&correct_e).await?;
+            let wrong = wd.get_element_text(&wrong).await?;
             correct = Some(
                 num_list(&correct_t)
                     .into_iter()
