1739723ce5004121315bec2aa92bb1a928365986
msizanoen1 <qtmlabs@protonmail.com>
2020-03-31 12:28:00 +0700

    Fix down.sql for revert of drop_legacy

--- migrations/2020-03-20-054556_drop_legacy/down.sql
+++ migrations/2020-03-20-054556_drop_legacy/down.sql
@@ -1,4 +1,7 @@
-CREATE TABLE answers_revert_tmp (
+CREATE TEMPORARY TABLE answers_revert_tmp
+AS SELECT * FROM answers;
+DROP TABLE answers;
+CREATE TABLE answers (
     question_id INTEGER PRIMARY KEY REFERENCES question_strings (question_id),
     answer1 INTEGER REFERENCES answer_strings (answer_id),
     answer2 INTEGER REFERENCES answer_strings (answer_id),
@@ -10,8 +13,7 @@ CREATE TABLE answers_revert_tmp (
     valid_answers INTEGER ARRAY NOT NULL DEFAULT '{}'
 );
 INSERT INTO
-    answers_revert_tmp (question_id, answer_used, reviewed, group_, valid_answers)
-SELECT * FROM answers;
-DROP TABLE answers;
-ALTER TABLE answers_revert_tmp
-    RENAME TO answers;
+    answers (question_id, answer_used, reviewed, group_, valid_answers)
+SELECT * FROM answers_revert_tmp;
+CREATE INDEX answer_group_idx ON answers (group_);
+CREATE INDEX answers_reviewed_idx ON answers (reviewed);
