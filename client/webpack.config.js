94d1ebb59ea8332a3f317c524b0670334c837df2
msizanoen1 <qtmlabs@protonmail.com>
2020-04-01 18:24:57 +0700

    Add build-babel and build-devel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,30 +12,38 @@ const babelOptions = {
   ]
 };
 
-module.exports = {
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: babelOptions
-          },
-          {
-            loader: "ts-loader"
-          }
-        ],
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".ts", ".tsx"]
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
+module.exports = function(env, argv) {
+  const babel = env != null && env.babel == "true";
+  return {
+    entry: "./src/index.ts",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: (() => {
+            let use = [];
+            if (babel) {
+              use.push({
+                loader: "babel-loader",
+                options: babelOptions
+              });
+            }
+            use.push({
+              loader: "ts-loader"
+            });
+            return use;
+          })(),
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".js", ".ts", ".tsx"]
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    devtool: "source-map"
+  };
 };
